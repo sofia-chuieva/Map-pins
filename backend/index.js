f8a5d6802f998734b8dfacfceba45f6c4e50261f
Sonya Chuyeva <sschuyeva@gmail.com>
2022-11-20 19:55:08 +0200

    add db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("mongoDB connected");
+  })
+  .catch((error) => console.log(error));
 
 app.listen(3009, () => {
   console.log("server is running");
