0f3b5d66071446712844e3378748c0a493ec25ce
sschu <sschuyeva@gmail.com>
2023-01-17 20:01:03 +0000

    add login,register,bcrypt package

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const app = express();
+const userRoute = require("./routes/users");
+const pinRoute = require("./routes/pins");
+app.use(express.json());
 
 dotenv.config();
-
-const app = express();
 
 mongoose
   .connect(process.env.MONGO_URL, { useNewUrlParser: true })
@@ -12,6 +14,9 @@ mongoose
     console.log("mongoDB connected");
   })
   .catch((error) => console.log(error));
+
+app.use("/api/users", userRoute);
+app.use("/api/pins", pinRoute);
 
 app.listen(3009, () => {
   console.log("server is running");
