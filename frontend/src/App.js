7caedd1e91d39e1b26f6fba6d2802a82fc4cbb18
sschu <sschuyeva@gmail.com>
2023-01-18 21:12:53 +0000

    add material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,10 @@
-/*import Map from "react-map-gl";
-import * as React from "react";
+import React, { useState } from "react";
+import Map, { Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 function App() {
-  const [viewport, setViewport] = React.useState({
-    width: "100vw",
-    height: "100vh",
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8,
-  });
-  return (
-    <Map
-      {...viewport}
-      onMove={(evt) => setViewport(evt.viewState)}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-      mapboxAccessToken="pk.eyJ1Ijoic3VwZXJzb25pazAwMiIsImEiOiJjbGQyNDdtYXcwNTY0M3FvMHR5MWxlOTNhIn0.7p9dBpNC0r7agArQmP4H0w"
-    />
-  );
-}
-*/
-import * as React from "react";
-import Map from "react-map-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-function App() {
-  const [viewState, setViewState] = React.useState({
+  const [viewState, setViewState] = useState({
     longitude: 17,
     latitude: 46,
     zoom: 6,
@@ -37,7 +17,16 @@ function App() {
       style={{ width: "100vw", height: "100vh" }}
       mapboxAccessToken="pk.eyJ1Ijoic3VwZXJzb25pazAwMiIsImEiOiJjbGQyNDdtYXcwNTY0M3FvMHR5MWxlOTNhIn0.7p9dBpNC0r7agArQmP4H0w"
       mapStyle="mapbox://styles/mapbox/streets-v9"
-    />
+    >
+      <Marker
+        longitude={2.294694}
+        latitude={48.858093}
+        offsetLeft={-20}
+        offsetTop={-10}
+      >
+        <LocationOnIcon fontSize="large" />
+      </Marker>
+    </Map>
   );
 }
 export default App;
