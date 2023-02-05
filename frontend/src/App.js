342d61a64a2571c8d2f340581117f5a88e8ac0e5
sschu <sschuyeva@gmail.com>
2023-02-05 18:02:37 +0000

    add api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ function App() {
 
   const getPins = async () => {
     try {
-      const res = await axios.get(`${process.env.REACT_APP_URL}/pins`);
+      const res = await axios.get("https://map-pin-app.vercel.app/api/pins");
       setPins(res.data);
     } catch (error) {
       console.log(error);
@@ -60,7 +60,7 @@ function App() {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`${process.env.REACT_APP_URL}/pins/${id}`);
+      await axios.delete(`https://map-pin-app.vercel.app/api/pins/${id}`);
       getPins();
     } catch (error) {
       console.log(error);
@@ -71,7 +71,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const res = await axios.post(`${process.env.REACT_APP_URL}/pins`, {
+      const res = await axios.post("https://map-pin-app.vercel.app/api/pins", {
         username: currentUser,
         title,
         description,
